bd5d729c3df44f36b61941c04430421747ee5ec4
larsbratholm <lab@lab.(none)>
2012-11-23 16:25:01 +0100

    made changes for how proline and glycine output will be. Changed camshift from using HN to H.

--- reader.py
+++ reader.py
@@ -13,15 +13,21 @@ def cam_(filename):			#outputs a temporary file of {(res.no, CS_type): CS}, with
 	list_=lines(filename)
 	d={}
 	for line in range(2,len(list_)):
-		try:
-			for i in range(2,len(list_[2])):
-				if float(list_[line][i])!=0:
-					d.setdefault((line-2,list_[0][i]), list_[line][i])
-				else:
-					d.setdefault((int(list_[line][0])-1,list_[0][i]), 'N/A')
-		except IndexError:
-			pass
-	return d, line
+		for i in range(2,len(list_[2])):
+			l=list_[0][i]
+			if l=='HN':
+				l='H'
+			if float(list_[line][i])!=0:
+				d[(line-2,l)]= list_[line][i]
+			else:
+				d[(line-2,l)]= 'N/A'
+		if list_[line][1]=='GLY' and float(list_[line][2])!=0.: #second criteria prevents that the first and last element in camshift gets edited if it's all zero.
+			d[(line-2,'CB')]= -100		#sets CB to -100. This makes sure that all GLY gets paired together.
+		if list_[line][1]=='PRO' and float(list_[line][2])!=0.: #float due to camshift being unconsistant with these things for some reason.
+			print list_[line][2]
+			d[(line-2,'H')]= -100
+			d[(line-2,'N')]= -100
+	return d, line-1
 
 
 def str_(filename):			#outputs a temporary file of {(res.no, CS_type): CS}, with LESS unnecesary data :O
@@ -40,6 +46,12 @@ def str_(filename):			#outputs a temporary file of {(res.no, CS_type): CS}, with
 				l+=1
 				d.setdefault((l,list_[line][7]), float(list_[line][10]))
 				structure.append(list_[line][6])
+		if list_[line][6]=='GLY' and  list_[line][7]=='HA3':#Have no idea what the HA entry in cs files is, since there is HA, HA2 AND HA3 values listed. So I use HA3 as HA for GLY.
+			d[l,'HA']= d[l,'HA3']
+			d[l,'CB']=-100
+		if list_[line][6]=='PRO':
+			d[l,'H']=-100
+			d[l,'N']=-100
 	return d, structure, l+1
 
 
@@ -49,11 +61,26 @@ def cs_(filename):
 	for line in range(len(list_)):
 		if line==0:
 			l=0
-			d.setdefault((l,list_[line][3]), float(list_[line][4]))
+			d[l,list_[line][3]]= float(list_[line][4])
 		else:
 			if list_[line][1] == list_[line-1][1]:
-				d.setdefault((l,list_[line][3]), float(list_[line][4]))
+				d[l,list_[line][3]]= float(list_[line][4])
 			else:
 				l+=1
-				d.setdefault((l,list_[line][3]), float(list_[line][4]))
+				d[l,list_[line][3]]= float(list_[line][4])
+		if list_[line][2]=='GLY' and  list_[line][3]=='HA3':#Have no idea what the HA entry in cs files is, since there is HA, HA2 AND HA3 values listed. So I use HA3 as HA for GLY.
+			d[l,'HA']= d[l,'HA3']
+			d[l,'CB']=-100
+		if list_[line][2]=='PRO':
+			d[l,'H']=-100
+			d[l,'N']=-100
 	return d, l+1
+
+A,L_A=cs_('1HCB.cs')
+B,L_B=cam_('1HCB.cam')
+C,structure, L_C=str_('1HCB.str')
+
+print L_A, L_B, L_C
+print A[(0,'H')],A[(2,'HA')], A[(2,'CB')], A[(37,'C')], A[(37,'H')], A[(37,'N')]
+print B[(0,'H')],B[(2,'HA')], B[(2,'CB')], B[(37,'C')], B[(37,'H')], B[(37,'N')]
+print C[(0,'H')],C[(2,'HA')], C[(2,'CB')], C[(37,'C')], C[(37,'H')], C[(37,'N')]
