f833b9c0ccbdfa29262c2e5735d9fcb7358e07da
larsbratholm <lab@lab.(none)>
2012-11-23 16:41:01 +0100

    fixed minor errors

--- reader.py
+++ reader.py
@@ -18,12 +18,12 @@ def cam_(filename):			#outputs a temporary file of {(res.no, CS_type): CS}, with
 			if l=='HN':
 				l='H'
 			if float(list_[line][i])!=0:
-				d[(line-2,l)]= list_[line][i]
+				d[(line-2,l)]= float(list_[line][i])
 			else:
 				d[(line-2,l)]= 'N/A'
-		if list_[line][1]=='GLY' and float(list_[line][2])!=0.: #second criteria prevents that the first and last element in camshift gets edited if it's all zero.
+		if list_[line][1]=='GLY' and float(list_[line][2])!=0: #second criteria prevents that the first and last element in camshift gets edited if it's all zero.
 			d[(line-2,'CB')]= -100		#sets CB to -100. This makes sure that all GLY gets paired together.
-		if list_[line][1]=='PRO' and float(list_[line][2])!=0.: #float due to camshift being unconsistant with these things for some reason.
+		if list_[line][1]=='PRO' and float(list_[line][2])!=0: #float due to camshift being unconsistant with these things for some reason.
 			print list_[line][2]
 			d[(line-2,'H')]= -100
 			d[(line-2,'N')]= -100
@@ -36,15 +36,15 @@ def str_(filename):			#outputs a temporary file of {(res.no, CS_type): CS}, with
 	structure=[]
 	for line in range(len(list_)):
 		if line==0:
-			d.setdefault((line,list_[line][7]), float(list_[line][10]))
+			d[(line,list_[line][7])]= float(list_[line][10])
 			structure.append(list_[line][6])
 			l=0
 		else:
 			if list_[line][5] == list_[line-1][5]:
-				d.setdefault((l,list_[line][7]), float(list_[line][10]))
+				d[(l,list_[line][7])]= float(list_[line][10])
 			else:
 				l+=1
-				d.setdefault((l,list_[line][7]), float(list_[line][10]))
+				d[(l,list_[line][7])]= float(list_[line][10])
 				structure.append(list_[line][6])
 		if list_[line][6]=='GLY' and  list_[line][7]=='HA3':#Have no idea what the HA entry in cs files is, since there is HA, HA2 AND HA3 values listed. So I use HA3 as HA for GLY.
 			d[l,'HA']= d[l,'HA3']
